c4e25314f9025dda36863d4524839fe7cad7267e
annapichnenko <annapichnenko@gmail.com>
2022-11-28 19:05:30 +0600

    SERVER

---
+++ router.js
@@ -0,0 +1,17 @@
+import Router from "express";
+import LoginController from "./Controllers/LoginController.js";
+import RoomsController from "./Controllers/RoomsController.js";
+import DataController from "./DataController.js";
+import RegController from "./RegController.js";
+
+const router = new Router();
+
+router.get("/datas", DataController.getTitle);
+router.post("/datas", DataController.create);
+router.post("/registration", RegController.create);
+router.get("/users", RegController.getAll);
+router.post("/login", LoginController.login);
+router.get("/rooms", RoomsController.getAll);
+router.post("/rooms", RoomsController.create);
+
+export default router;
